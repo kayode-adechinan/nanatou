f9cc0ee8205ba603e8fc529b9b7f3a900d81f3da
kayode-adechinan <kayode.adechinan@gmail.com>
2018-11-26 16:25:58 -0500

    aaaa

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="nanatou",
+    version="0.0.1",
+    author="Kayode Adechinan",
+    author_email="kayode.adechinan@gmail.com",
+    description="to my lover",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests',
+      ],
+    license='MIT',
+    zip_safe=False
+)
